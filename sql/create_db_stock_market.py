b8924a86471d64849ab13d57c0715529e438e6d7
Mcruz-G <macruzgom@gmail.com>
2023-06-08 20:07:58 -0600

    Added the code to create the db

--- sql/create_db_stock_market.py
+++ sql/create_db_stock_market.py
@@ -1,59 +1,72 @@
 import sqlite3
 import random
+from faker import Faker
 
 def create_db_stock_market():
-    # Connect to the database or create it if it doesn't exist
-    conn = sqlite3.connect('finance_database.db')
-    cursor = conn.cursor()
+
+    # Create a connection to a SQLite database
+    conn = sqlite3.connect('databases/stock_market.db')
+    cur = conn.cursor()
 
     # Create the tables
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS stocks (
-            id INTEGER PRIMARY KEY,
-            symbol TEXT,
-            company TEXT,
-            price REAL,
-            quantity INTEGER
-        )
-    ''')
+    cur.execute("""
+    CREATE TABLE Companies(
+        company_id INTEGER PRIMARY KEY,
+        company_name TEXT,
+        sector TEXT
+    );
+    """)
 
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS transactions (
-            id INTEGER PRIMARY KEY,
-            stock_id INTEGER,
-            date TEXT,
-            type TEXT,
-            price REAL,
-            quantity INTEGER,
-            FOREIGN KEY(stock_id) REFERENCES stocks(id)
-        )
-    ''')
+    cur.execute("""
+    CREATE TABLE Stocks(
+        stock_id INTEGER PRIMARY KEY,
+        company_id INTEGER,
+        ticker_symbol TEXT,
+        price REAL,
+        market_cap REAL,
+        FOREIGN KEY(company_id) REFERENCES Companies(company_id)
+    );
+    """)
 
-    # Generate random data for the stocks table
-    stocks = [
-        ('AAPL', 'Apple Inc.', 150.23, 100),
-        ('GOOGL', 'Alphabet Inc.', 2300.45, 50),
-        ('MSFT', 'Microsoft Corporation', 250.67, 75),
-        ('AMZN', 'Amazon.com, Inc.', 3500.12, 25),
-        ('TSLA', 'Tesla, Inc.', 600.34, 40)
-    ]
+    cur.execute("""
+    CREATE TABLE Transactions(
+        transaction_id INTEGER PRIMARY KEY,
+        stock_id INTEGER,
+        quantity INTEGER,
+        transaction_type TEXT,
+        transaction_date TEXT,
+        FOREIGN KEY(stock_id) REFERENCES Stocks(stock_id)
+    );
+    """)
 
-    cursor.executemany('INSERT INTO stocks (symbol, company, price, quantity) VALUES (?, ?, ?, ?)', stocks)
+    # Generate random data and load it into the tables
+    faker = Faker()
+    sectors = ['Tech', 'Finance', 'Healthcare', 'Energy', 'Consumer Goods']
+    transaction_types = ['BUY', 'SELL']
 
-    # Generate random data for the transactions table
-    transaction_types = ['Buy', 'Sell']
+    # Generate a list of market caps
+    market_caps = sorted([random.uniform(1000000000, 1000000000000) for _ in range(10)], reverse=True)
 
-    for stock_id in range(1, len(stocks) + 1):
-        for _ in range(5):
-            date = f'2023-06-{random.randint(1, 30):02d}'
+    for i in range(10):
+        company_name = faker.company()
+        sector = random.choice(sectors)
+        cur.execute(f"INSERT INTO Companies (company_name, sector) VALUES ('{company_name}', '{sector}');")
+
+        # Assign market cap to stock
+        ticker_symbol = faker.lexify(text="???")
+        price = random.uniform(10, 1000)
+        market_cap = market_caps[i]
+        cur.execute(f"INSERT INTO Stocks (company_id, ticker_symbol, price, market_cap) VALUES ({i}, '{ticker_symbol}', {price}, {market_cap});")
+
+        for _ in range(10):  # Assuming there are 10 transactions for each stock
+            quantity = random.randint(1, 100)
             transaction_type = random.choice(transaction_types)
-            price = random.uniform(0.8, 1.2) * stocks[stock_id - 1][2]  # Random variation in price
-            quantity = random.randint(5, 20)
-            
-            cursor.execute('INSERT INTO transactions (stock_id, date, type, price, quantity) VALUES (?, ?, ?, ?, ?)',
-                        (stock_id, date, transaction_type, price, quantity))
+            transaction_date = faker.date_between(start_date='-1y', end_date='today')
+            cur.execute(f"INSERT INTO Transactions (stock_id, quantity, transaction_type, transaction_date) VALUES ({i}, {quantity}, '{transaction_type}', '{transaction_date}');")
 
-    # Commit the changes and close the connection
+    # Commit changes and close connection
     conn.commit()
     conn.close()
 
+
+create_db_stock_market()
